0aad46c9a2efaea476db5f218c911bf1c3daa53a
Amando Filipe <amando96@gmail.com>
2015-01-10 13:28:06 +0000

    Removed localhost credentials

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 	$host = 'localhost';
-	$dbname = 'niobo_niobo';
+	$dbname = 'user';
 	$charset = 'utf8';
-	$user = 'niobo_niobo';
-	$pass = '151@Nio0513';
+	$user = 'user';
+	$pass = 'pass';
 	try {
 		$db = new PDO('mysql:host='.$host.';dbname='.$dbname.';charset='.$charset, $user, $pass);
 	} catch(PDOException $ex) {
